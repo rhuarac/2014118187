89d2457ad7b1f38eb3f0a21ce4a5943115873158
Rodrigo <rhuarac@gmail.com>
2017-05-31 13:44:11 -0500

    PC FINAL

---
+++ 2014118187-CON/2014118187-ENT/Entities/Cuenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014118187_ENT.Entities
+{
+    public class Cuenta
+    {
+        public int NumeroCuenta { get; set; }
+        public int Pin { get; set; }
+
+        //Base de Datos
+        public int BaseDatosId { get; set; }
+        public BasedeDatos BasedeDatos { get; set; }
+    }
+}
---
+++ 2014118187-CON/2014118187-ENT/Entities/Retiro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014118187_ENT.Entities
+{
+    public class Retiro
+    {
+        public int RetiroId { get; set; }
+
+        //Pantalla
+        public int PantallaId { get; set; }
+        public Pantalla Pantalla { get; set; }
+
+        //Teclado
+        public int TecladoId { get; set; }
+        public Teclado Teclado { get; set; }
+
+        //DispensadorEfectivo
+        public int DispensadorEfectivoId { get; set; }
+        public DispensadorEfectivo DispensadorEfectivo { get; set; }
+
+        //Retiro
+        public int BasedeDatosId { get; set; }
+        public BasedeDatos BasedeDatos { get; set; }
+
+        //ATM
+        public int ATMId { get; set; }
+        public ATM ATM { get; set; }
+    }
+}
